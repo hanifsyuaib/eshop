4c369e630e89d640ca9c40b71491a94b0212ed5d
Hanif Ibrahim Syuaib <hanifsyuaib@gmail.com>
2024-02-14 13:04:30 +0700

    Change/fix duplicate

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService service;
+    private static final String REDIRECT_TO_LIST = "redirect:list";
 
     public ProductController(ProductService service) {
         this.service = service;
@@ -29,7 +30,7 @@ public class ProductController {
     @PostMapping("/create")
     public String createProductPost(@ModelAttribute Product product, Model model) {
         service.create(product);
-        return "redirect:list";
+        return REDIRECT_TO_LIST;
     }
 
     @GetMapping("/list")
@@ -49,12 +50,12 @@ public class ProductController {
     @PostMapping("/edit")
     public String editProductPost(@ModelAttribute Product product, Model model) {
         service.edit(product);
-        return "redirect:list";
+        return REDIRECT_TO_LIST;
     }
 
     @GetMapping("/delete")
     public String deleteProduct(@RequestParam String productName, Model model) {
         service.delete(productName);
-        return "redirect:list";
+        return REDIRECT_TO_LIST;
     }
 }

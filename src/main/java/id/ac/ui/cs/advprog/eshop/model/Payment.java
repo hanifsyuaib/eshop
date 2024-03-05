a3bbb5716c992b85e51a63231ad863205a367a93
Hanif Ibrahim Syuaib <hanifsyuaib@gmail.com>
2024-03-05 11:49:21 +0700

    [REFACTOR] Add PaymentStatus enum

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -16,26 +18,30 @@ public class Payment {
 
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
         this.id = id;
-
-        String[] methodList = {"VOUCHER_CODE", "CASH_ON_DELIVERY"};
-        if (Arrays.stream(methodList).noneMatch(item -> (item.equals(method)))) {
-            throw new IllegalArgumentException();
-        } else {
-            this.method = method;
-        }
-
-        String[] statusList = {"SUCCESS", "REJECTED"};
-        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
-            throw new IllegalArgumentException();
-        } else {
-            this.status = status;
-        }
+        this.setMethod(method);
+        this.setStatus(status);
 
         String[] paymentDataList = {"voucherCode", "address", "deliveryFee"};
         if (Arrays.stream(paymentDataList).noneMatch(item -> (paymentData.containsKey(item)))) {
             throw new IllegalArgumentException();
         } else {
             this.paymentData = paymentData;
+        }
+    }
+
+    public void setMethod(String method) {
+        if (PaymentMethod.contains(method)) {
+            this.method = method;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void setStatus(String status) {
+        if (PaymentStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
         }
     }
 }
